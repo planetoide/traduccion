26ae8a1f51e7c4a3927ca3ad7d4029510eaf2a68
Planetoide <adcircus@gmail.com>
2021-03-11 10:44:32 -0600

    aplicacion de traduccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'translate';
+
+  supportLanguages = ['es', 'en', 'fr', 'ja'];
+
+  constructor(private _translateService: TranslateService) {
+    this._translateService.addLangs(this.supportLanguages);
+    //establece un lenguaje por defecto
+    // this._translateService.setDefaultLang('es');
+
+    // Obtiene el lenguaje del navegador por defecto
+    const browserLang = this._translateService.getBrowserLang();
+    // Usa por defecto el lenguaje del navegador
+    this._translateService.use(browserLang)
+  }
+
+  useLang(lang: string) {
+    this._translateService.use(lang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TranslateModule, TranslateLoader, TranslateService } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ChildrenComponent } from './children/children.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildrenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => { return new TranslateHttpLoader(http, './assets/i18n/', '.json'); },
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
